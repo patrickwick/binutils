31180b8f3bba9ef55bdc868913c20e84aa8d8c62
Patrick Wickenhaeuser <35903594+patrickwick@users.noreply.github.com>
2024-10-19 00:56:00 +0200

    Add expectExit assertion to test fatal errors

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ const readelf = @import("readelf.zig");
 const objdump = @import("objdump.zig");
 const objcopy = @import("objcopy.zig");
 
+const FATAL_ERROR = 1;
+
 pub fn main() !void {
     const std_out = std.io.getStdOut().writer().any();
 
@@ -18,11 +20,7 @@ pub fn main() !void {
     if (args.len < 1) fatal("expected executable path argument", .{});
     args = args[1..];
 
-    const command = parseCommand(std_out, args) catch |err| switch (err) {
-        CommandParsingError.MissingCommand => fatalPrintUsage(std_out, "command argument required", .{}),
-        CommandParsingError.UnrecognizedCommand => fatalPrintUsage(std_out, "unrecognized command: '{s}'", .{args[0]}),
-    };
-
+    const command = parseCommand(std_out, args);
     switch (command) {
         .readelf => |options| readelf.readelf(options),
         .objdump => |options| objdump.objdump(options),
@@ -55,24 +53,22 @@ const Command = union(enum) {
     objcopy: objcopy.ObjCopyOptions,
 };
 
-const CommandParsingError = error{
-    MissingCommand,
-    UnrecognizedCommand,
-};
-
-fn parseCommand(out: std.io.AnyWriter, args: []const []const u8) CommandParsingError!Command {
-    if (args.len < 1) return CommandParsingError.MissingCommand;
+fn parseCommand(out: std.io.AnyWriter, args: []const []const u8) Command {
+    if (args.len < 1) fatalPrintUsage(out, "command argument required", .{});
     const command = args[0];
     if (std.mem.eql(u8, command, "readelf")) return Command{ .readelf = parseReadelf(out, args[1..]) };
     if (std.mem.eql(u8, command, "objdump")) return Command{ .objdump = parseObjdump(out, args[1..]) };
     if (std.mem.eql(u8, command, "objcopy")) return Command{ .objcopy = parseObjcopy(out, args[1..]) };
-    return CommandParsingError.UnrecognizedCommand;
+    fatalPrintUsage(out, "unrecognized command: '{s}'", .{args[0]});
 }
 
 fn parseReadelf(out: std.io.AnyWriter, args: []const []const u8) readelf.ReadElfOptions {
     _ = out;
-    _ = args;
-    return .{};
+    if (args.len < 1) fatal("expected input file path", .{});
+
+    return .{
+        .file_path = args[0],
+    };
 }
 
 fn parseObjdump(out: std.io.AnyWriter, args: []const []const u8) objdump.ObjDumpOptions {
@@ -89,24 +85,79 @@ fn parseObjcopy(out: std.io.AnyWriter, args: []const []const u8) objcopy.ObjCopy
 
 pub fn fatal(comptime format: []const u8, args: anytype) noreturn {
     std.log.err(format, args);
-    std.process.exit(1);
+    std.process.exit(FATAL_ERROR);
 }
 
 pub fn fatalPrintUsage(out: std.io.AnyWriter, comptime format: []const u8, args: anytype) noreturn {
     std.log.err(format, args);
     printUsage(out);
-    std.process.exit(1);
+    std.process.exit(FATAL_ERROR);
 }
 
 const t = std.testing;
 
+/// Expect the function to cause the process to exit with a non-zero exit code.
+fn expectExit(comptime expected_exit_code: u32, function: anytype) !void {
+    if (@import("builtin").os.tag != .linux) {
+        std.log.info("expectExit is only executed on linux", .{});
+        return;
+    }
+    if (expected_exit_code == 0) @compileError("Only non-zero exit codes are supported");
+
+    // fork the process: child executes the function, parent waits for the exit code
+    const child_pid = try std.posix.fork();
+    if (child_pid == 0) {
+        // child
+        function() catch {};
+        std.process.exit(0);
+    }
+
+    // parent
+    const has_not_changed = 0;
+    var status: u32 = 0;
+    while (std.os.linux.waitpid(child_pid, &status, 0) == has_not_changed or !std.os.linux.W.IFEXITED(status)) {}
+    const exit_code = std.os.linux.W.EXITSTATUS(status);
+    if (exit_code != expected_exit_code) {
+        t.expectEqual(expected_exit_code, exit_code) catch |err| {
+            std.log.err("Function did not exit with the expected error code", .{});
+
+            const max_frames = 20;
+            var addresses: [max_frames]usize = [1]usize{0} ** max_frames;
+            var stack_trace = std.builtin.StackTrace{
+                .instruction_addresses = &addresses,
+                .index = 0,
+            };
+            std.debug.captureStackTrace(@returnAddress(), &stack_trace);
+            std.debug.dumpStackTrace(stack_trace);
+
+            return err;
+        };
+    }
+}
+
 test parseCommand {
+    try expectExit(FATAL_ERROR, struct {
+        fn F() !void {
+            _ = parseCommand(std.io.null_writer.any(), &.{});
+        }
+    }.F);
+
+    try expectExit(FATAL_ERROR, struct {
+        fn F() !void {
+            _ = parseCommand(std.io.null_writer.any(), &.{""});
+        }
+    }.F);
+
+    try expectExit(FATAL_ERROR, struct {
+        fn F() !void {
+            _ = parseCommand(std.io.null_writer.any(), &.{"discombobulate"});
+        }
+    }.F);
+
     const writer = std.io.null_writer.any();
-    try t.expect(std.meta.activeTag(try parseCommand(writer, &.{"readelf"})) == .readelf);
-    try t.expect(std.meta.activeTag(try parseCommand(writer, &.{"objdump"})) == .objdump);
-    try t.expect(std.meta.activeTag(try parseCommand(writer, &.{"objcopy"})) == .objcopy);
-    try t.expectError(CommandParsingError.MissingCommand, parseCommand(writer, &.{}));
-    try t.expectError(CommandParsingError.UnrecognizedCommand, parseCommand(writer, &.{"discombobulate"}));
+    try t.expect(std.meta.activeTag(parseCommand(writer, &.{ "readelf", "./file" })) == .readelf);
+    try t.expect(std.meta.activeTag(parseCommand(writer, &.{ "objdump", "file_path" })) == .objdump);
+    try t.expect(std.meta.activeTag(parseCommand(writer, &.{ "objcopy", "/home/abc/input", "./output" })) == .objcopy);
 }
 
 test {
--- src/readelf.zig
+++ src/readelf.zig
@@ -1,4 +1,6 @@
-pub const ReadElfOptions = struct {};
+pub const ReadElfOptions = struct {
+    file_path: []const u8,
+};
 
 pub fn readelf(options: ReadElfOptions) void {
     _ = options;

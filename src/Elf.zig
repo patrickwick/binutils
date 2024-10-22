75c8f4e8c20413d4e1c8be0f763070983cd53e0b
Patrick Wickenhaeuser <35903594+patrickwick@users.noreply.github.com>
2024-10-22 14:35:30 +0200

    Inline Section and ProgramSegment in Elf.zig since they're simple enough

--- src/Elf.zig
+++ src/Elf.zig
@@ -2,8 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 const testing = @import("testing.zig");
-const Section = @import("Section.zig").Section;
-const ProgramSegment = @import("ProgramSegment.zig").ProgramSegment;
 
 const FATAL_EXIT_CODE = 1;
 
@@ -47,6 +45,122 @@ pub const EIdent = struct {
 comptime {
     // TODO: verify that each field is a byte in size => important if source endianess does not match native endianness
 }
+
+pub const Section = struct {
+    pub const Handle = u64;
+
+    // copy from the input file
+    pub const InputFileRange = struct {
+        offset: usize,
+        size: usize,
+    };
+
+    pub const NoBits = struct {
+        // TODO: are those required at all?
+        // * the information is stored in the header, so it's redundant and can go out of sync.
+        // * rename: they're **not** file offsets / sizes, they're process virtual addresses / memory size when laoded.
+        offset: usize,
+        size: usize,
+    };
+
+    // new data written into new sections that did not exist in the input
+    pub const Data = []const u8;
+
+    /// Section contents have different sources and are only loaded and copied on demand.
+    /// For example, appending data to a section from the input file converts it from a file range to a modified heap allocated copy of the input section.
+    /// Unmodified sections remain file range references and are only read on demand when writing the ELF file.
+    pub const ContentSource = union(enum) {
+        /// Each section only references the input file range when read initially.
+        input_file_range: InputFileRange,
+        /// SHT_NOBITS sections (e.g.: .bss) are not contained in the file. All information is contained in the headers.
+        no_bits: NoBits,
+        /// Section content that is written programmatically.
+        data: Data,
+        /// Section content that is written programmatically and needs to be freed.
+        data_allocated: Data,
+
+        pub inline fn fileSize(self: *const @This()) usize {
+            return switch (self.*) {
+                .input_file_range => |range| range.size,
+                .no_bits => 0, // no size in ELF file, only at runtime
+                .data => |data| data.len,
+            };
+        }
+
+        pub inline fn headerSize(self: *const @This()) usize {
+            return switch (self.*) {
+                .input_file_range => |range| range.size,
+                .no_bits => |range| range.size,
+                .data => |data| data.len,
+            };
+        }
+    };
+
+    // unique handle to identify the section without comparing names
+    handle: Handle,
+
+    // TODO: sh_name, sh_offset and sh_size may go out of sync and are fixed during processing. Is this avoidable?
+    // * sh_name: if any section name before was changed / removed / added
+    // * sh_offset: if any section content before was resized, have updated alignment or are reordered
+    // * sh_size: section content is overwritten
+    // => could extract functions on these operations to always update all sections immediately or store the affected fields separately
+    header: std.elf.Shdr,
+    content: ContentSource,
+
+    allocator: std.mem.Allocator,
+
+    pub fn deinit(self: *@This()) void {
+        switch (self.content) {
+            .data_allocated => |data| self.allocator.free(data),
+            .input_file_range, .data, .no_bits => {},
+        }
+    }
+
+    pub fn readContent(self: *@This(), input: anytype, allocator: std.mem.Allocator) ![]const u8 {
+        comptime std.debug.assert(std.meta.hasMethod(@TypeOf(input), "seekableStream"));
+        comptime std.debug.assert(std.meta.hasMethod(@TypeOf(input), "reader"));
+
+        switch (self.content) {
+            .input_file_range => |range| {
+                self.allocator = allocator;
+                const data = try allocator.alloc(u8, range.size);
+                errdefer allocator.free(data);
+
+                try input.seekableStream().seekTo(range.offset);
+                const bytes_read = try input.reader().readAll(data);
+                if (bytes_read != data.len) return error.TruncatedElf;
+
+                // converts to data allocated once read, so it's not read from file again
+                self.content = .{ .data_allocated = data };
+
+                return data;
+            },
+            .no_bits => return "", // e.g.: .bss
+            .data, .data_allocated => |data| return data,
+        }
+    }
+
+    // Create the section header in the input file endianess
+    pub fn toShdr(self: *const @This(), output_endianess: std.builtin.Endian) std.elf.Shdr {
+        _ = output_endianess;
+        std.log.warn("TODO: apply endianess on copy if native does not match target", .{});
+        // std.mem.byteSwapAllFields(comptime S: type, ptr: *S)
+        return self.header;
+    }
+};
+
+pub const ProgramSegment = struct {
+    pub const SegmentMapping = std.ArrayList(Section.Handle);
+
+    // TODO: temporary copy of the input
+    // => represent section to segment mapping via handles and create
+    // the offset only when writing, so data does not go out of sync
+    program_header: std.elf.Phdr,
+
+    /// Section to segment mapping. A segment can reference 0 to n sections.
+    /// Does not support referencing a subrange of a section.
+    segment_mapping: SegmentMapping,
+};
 
 pub const Sections = std.ArrayList(Section);
 pub const ProgramSegments = std.ArrayList(ProgramSegment);
